9d4319e07cab53fe63f05b43bef56189fc595bd7
Nils Philippsen <nils@redhat.com>
2024-07-31 12:43:44 +0200

    Add --dry-run option
    Retrieve information from configured upstream repositories, but don’t
    attempt to change anything in JIRA.
    
    Related: #8
    
    Signed-off-by: Nils Philippsen <nils@redhat.com>

--- jira_sync/jira_wrapper.py
+++ jira_sync/jira_wrapper.py
@@ -27,27 +27,32 @@ class JIRA:
                                  Example: {"NEW": "1"}
     """
 
-    jira: jira.client.JIRA
+    jira: jira.client.JIRA | None
     project: str
     issue_type: str
     project_statuses: dict[Issue, dict[str, str]]
 
     def __init__(
         self,
-        url: str,
-        token: str,
+        url: str | None,
+        token: str | None,
         project: str,
         issue_type: str,
     ):
         """
         Object constructor.
 
+        Set url and token to None for dry-run (simulated) operation.
+
         :param url: URL to JIRA server
         :param token: Token to use for authentication
         :param project: Project to work with
         :param issue_type: Default issue type for creating issues
         """
-        self.jira = jira.client.JIRA(url, token_auth=token)
+        if url and token:
+            self.jira = jira.client.JIRA(url, token_auth=token)
+        else:
+            self.jira = None
         self.project = project
         self.issue_type = issue_type
         self.project_statuses = {}
@@ -62,9 +67,13 @@ class JIRA:
 
         :return: Retrieved issue or None
         """
+        if not self.jira:
+            return None
+
         # Replace special characters
         title = title.replace("[", "\\\\[")
         title = title.replace("]", "\\\\]")
+
         issues = cast(
             jira.client.ResultList[Issue],
             self.jira.search_issues(
@@ -99,6 +108,9 @@ class JIRA:
 
         :return: List of issues
         """
+        if not self.jira:
+            return []
+
         issues = cast(
             jira.client.ResultList[Issue],
             self.jira.search_issues(
@@ -126,6 +138,9 @@ class JIRA:
 
         :return: Issue object or None
         """
+        if not self.jira:
+            return None
+
         issue_dict = {
             "project": {"key": self.project},
             "summary": summary,
@@ -146,6 +161,9 @@ class JIRA:
 
         :return: A dictionary mapping status names to ids
         """
+        if not self.jira:
+            return {}
+
         if issue not in self.project_statuses:
             self.project_statuses[issue] = {
                 transition["name"]: transition["id"] for transition in self.jira.transitions(issue)
@@ -159,6 +177,9 @@ class JIRA:
         :param issue: Issue object
         :param status: New status to move to
         """
+        if not self.jira:
+            return
+
         if issue.fields.status.name != status:
             log.debug("Changing status to '%s' in ticket %s", status, issue.key)
             self.jira.transition_issue(issue, self._get_issue_transition_statuses(issue)[status])
@@ -170,6 +191,9 @@ class JIRA:
         :param issue: Issue object
         :param user: Username to assign to ticket
         """
+        if not self.jira:
+            return
+
         if user != getattr(issue.fields.assignee, "name", None):
             log.debug("Assigning user %s to %s", user, issue.key)
             self.jira.assign_issue(issue.id, user)
@@ -181,6 +205,9 @@ class JIRA:
         :param issue: Issue object
         :param label: Label to add
         """
+        if not self.jira:
+            return
+
         if label not in issue.fields.labels:
             log.debug("Adding label %s to %s", label, issue.key)
             issue.add_field_value("labels", label)
--- tests/test_jira_wrapper.py
+++ tests/test_jira_wrapper.py
@@ -6,11 +6,21 @@ import pytest
 from jira_sync import jira_wrapper
 
 
+def pytest_generate_tests(metafunc):
+    if "dry_run" in metafunc.fixturenames:
+        metafunc.parametrize("dry_run", (False, True), ids=("for-real", "dry-run"), indirect=True)
+
+
 @pytest.fixture
-def jira_params() -> dict[str, str | object]:
+def dry_run(request):
+    return request.param
+
+
+@pytest.fixture
+def jira_params(dry_run) -> dict[str, str | object]:
     return {
-        "url": "https://jira.example.com",
-        "token": object(),  # a sentinel
+        "url": "https://jira.example.com" if not dry_run else None,
+        "token": object() if not dry_run else None,  # a sentinel
         "project": "project",
         "issue_type": "towel",
     }
@@ -33,37 +43,49 @@ class TestJIRA:
         "NEWSTATUS": "abc345",
     }
 
-    def test___init__(self, jira_obj, jira_params, mocked_jira_pkg):
-        mocked_jira_pkg.client.JIRA.assert_called_with(
-            jira_params["url"], token_auth=jira_params["token"]
-        )
-        assert jira_obj.jira == mocked_jira_pkg.client.JIRA.return_value
+    def test___init__(self, dry_run, jira_obj, jira_params, mocked_jira_pkg):
+        if dry_run:
+            mocked_jira_pkg.client.JIRA.assert_not_called()
+            assert jira_obj.jira is None
+        else:
+            mocked_jira_pkg.client.JIRA.assert_called_with(
+                jira_params["url"], token_auth=jira_params["token"]
+            )
+            assert jira_obj.jira == mocked_jira_pkg.client.JIRA.return_value
         assert jira_obj.project == jira_params["project"]
         assert jira_obj.issue_type == jira_params["issue_type"]
 
     @pytest.mark.parametrize(
         "testcase", ("issues-found", "issues-found-inexact", "issues-not-found")
     )
-    def test_get_issue_by_link(self, testcase, jira_obj, jira_params):
+    def test_get_issue_by_link(self, testcase, dry_run, jira_obj, jira_params):
         issues_found = "issues-found" in testcase
         inexact = "inexact" in testcase
 
         ISSUE_URL = "https://foo.bar/issue/1"
-        if issues_found:
-            inexact_issue = mock.Mock()
-            inexact_issue.fields.description = f"{ISSUE_URL} + some garbage!\nSome\nmore\ntext."
 
-            issues = [inexact_issue]
+        if not dry_run:
+            if issues_found:
+                inexact_issue = mock.Mock()
+                inexact_issue.fields.description = f"{ISSUE_URL} + some garbage!\nSome\nmore\ntext."
 
-            if not inexact:
-                exact_issue = mock.Mock()
-                exact_issue.fields.description = f"{ISSUE_URL}\nSome\nmore\ntext."
-                issues.append(exact_issue)
-        else:
-            issues = []
-        jira_obj.jira.search_issues.return_value = issues
+                issues = [inexact_issue]
+
+                if not inexact:
+                    exact_issue = mock.Mock()
+                    exact_issue.fields.description = f"{ISSUE_URL}\nSome\nmore\ntext."
+                    issues.append(exact_issue)
+            else:
+                issues = []
+
+            jira_obj.jira.search_issues.return_value = issues
 
         retval = jira_obj.get_issue_by_link(url=ISSUE_URL, repo="test", title="Foo")
+
+        if dry_run:
+            assert jira_obj.jira is None
+            assert retval is None
+            return
 
         jira_obj.jira.search_issues.assert_called_once()
         (jql_str,), _ = jira_obj.jira.search_issues.call_args
@@ -71,7 +93,6 @@ class TestJIRA:
         snippets = [sn.strip() for sn in jql_str.split("AND")]
 
         assert f"project = {jira_params['project']}" in snippets
-        # FIXME: assert "summary ~ \"Foo\"" in snippets
         assert f'Description ~ "{ISSUE_URL}"' in snippets
         assert 'labels = "test"' in snippets
 
@@ -83,13 +104,20 @@ class TestJIRA:
         else:
             assert retval is None
 
-    def test_get_open_issues_by_label(self, jira_obj, jira_params):
+    def test_get_open_issues_by_label(self, dry_run, jira_obj, jira_params):
         ISSUE_URL = "https://foo.bar/issue/1"
-        issue = mock.Mock()
-        issue.fields.description = f"{ISSUE_URL}\nSome\nmore\ntext."
-        jira_obj.jira.search_issues.return_value = [issue]
+
+        if not dry_run:
+            issue = mock.Mock()
+            issue.fields.description = f"{ISSUE_URL}\nSome\nmore\ntext."
+            jira_obj.jira.search_issues.return_value = [issue]
 
         retval = jira_obj.get_open_issues_by_label(label="label")
+
+        if dry_run:
+            assert jira_obj.jira is None
+            assert retval == []
+            return
 
         assert retval == [issue]
 
@@ -103,17 +131,23 @@ class TestJIRA:
         assert "status not in (Done, Closed)" in snippets
 
     @pytest.mark.parametrize("success", (True, False), ids=("success", "failure"))
-    def test_create_issue(self, success, mocked_jira_pkg, jira_obj, jira_params):
-        if success:
-            issue_sentinel = object()
-            jira_obj.jira.create_issue.return_value = issue_sentinel = object()
-        else:
-            mocked_jira_pkg.exceptions.JIRAError = RuntimeError
-            jira_obj.jira.create_issue.side_effect = RuntimeError("BOO")
+    def test_create_issue(self, success, dry_run, mocked_jira_pkg, jira_obj, jira_params):
+        if not dry_run:
+            if success:
+                issue_sentinel = object()
+                jira_obj.jira.create_issue.return_value = issue_sentinel = object()
+            else:
+                mocked_jira_pkg.exceptions.JIRAError = RuntimeError
+                jira_obj.jira.create_issue.side_effect = RuntimeError("BOO")
 
         retval = jira_obj.create_issue(
             summary="summary", description="description", url="url", label="label"
         )
+
+        if dry_run:
+            assert jira_obj.jira is None
+            assert retval is None
+            return
 
         if success:
             assert retval == issue_sentinel
@@ -121,8 +155,13 @@ class TestJIRA:
             assert retval is None
 
     @pytest.mark.parametrize("cold_cache", (True, False), ids=("cold-cache", "hot-cache"))
-    def test__get_issue_transition_statuses(self, cold_cache, jira_obj):
+    def test__get_issue_transition_statuses(self, cold_cache, dry_run, jira_obj):
         issue_sentinel = object()
+
+        if dry_run:
+            assert jira_obj.jira is None
+            assert jira_obj._get_issue_transition_statuses(issue_sentinel) == {}
+            return
 
         with mock.patch.dict(jira_obj.project_statuses, clear=True):
             if cold_cache:
@@ -140,7 +179,7 @@ class TestJIRA:
             jira_obj.jira.transitions.assert_not_called()
 
     @pytest.mark.parametrize("needs_transition", (True, False), ids=("needs-transition", "noop"))
-    def test_transition_issue(self, needs_transition, jira_obj, caplog):
+    def test_transition_issue(self, needs_transition, dry_run, jira_obj, caplog):
         issue = mock.Mock(key="KEY")
         if needs_transition:
             issue.fields.status.name = "OLDSTATUS"
@@ -150,6 +189,10 @@ class TestJIRA:
         with caplog.at_level("DEBUG"), mock.patch.dict(jira_obj.project_statuses, clear=True):
             jira_obj.project_statuses[issue] = self.ISSUE_STATUSES
             jira_obj.transition_issue(issue, "NEWSTATUS")
+
+        if dry_run:
+            assert jira_obj.jira is None
+            return
 
         if needs_transition:
             assert "Changing status to 'NEWSTATUS' in ticket KEY" in caplog.text
@@ -168,7 +211,7 @@ class TestJIRA:
         ),
         ids=("assign", "reassign", "noop"),
     )
-    def test_assign_to_issue(self, assignee_set, needs_assignment, jira_obj, caplog):
+    def test_assign_to_issue(self, assignee_set, needs_assignment, dry_run, jira_obj, caplog):
         issue = mock.Mock(key="KEY")
         if not assignee_set:
             issue.fields.assignee = None
@@ -181,6 +224,10 @@ class TestJIRA:
         with caplog.at_level("DEBUG"):
             jira_obj.assign_to_issue(issue=issue, user="newname")
 
+        if dry_run:
+            assert jira_obj.jira is None
+            return
+
         if needs_assignment:
             assert "Assigning user newname to KEY" in caplog.text
             jira_obj.jira.assign_issue.assert_called_once_with(issue.id, "newname")
@@ -189,7 +236,7 @@ class TestJIRA:
             jira_obj.jira.assign_issue.assert_not_called()
 
     @pytest.mark.parametrize("needs_labeling", (True, False), ids=("needs-labeling", "noop"))
-    def test_add_label(self, needs_labeling, jira_obj, caplog):
+    def test_add_label(self, needs_labeling, dry_run, jira_obj, caplog):
         issue = mock.Mock(key="KEY")
         issue.fields.labels = ["OLDLABEL"]
         if not needs_labeling:
@@ -197,6 +244,10 @@ class TestJIRA:
 
         with caplog.at_level("DEBUG"):
             jira_obj.add_label(issue, "NEWLABEL")
+
+        if dry_run:
+            assert jira_obj.jira is None
+            return
 
         if needs_labeling:
             assert "Adding label NEWLABEL to KEY" in caplog.text
